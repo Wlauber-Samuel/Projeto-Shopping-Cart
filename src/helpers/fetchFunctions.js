5638da528b8c0edf88bddb477f89da0cc2029e25
Wlauber <wlaubersamuel@gmail.com>
2022-11-28 19:22:18 -0300

    requisito 1 e 2

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,17 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList = (item) => {
+  if (!item) {
+    throw new Error('Termo de busca não informado');
+  }
+  const endpoint = `https://api.mercadolibre.com/sites/MLB/search?q=${item}`;
+  const { results } = data(endpoint);
+  return results;
+};
+
+const data = async (endpoint) => {
+  const response = await fetch(endpoint);
+  const json = await response.json();
+  return json;
 };
--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -2,20 +2,23 @@ import './mocks/fetchSimulator';
 import { fetchProductsList } from '../helpers/fetchFunctions';
 import computadorSearch from './mocks/search';
 
-// implemente seus testes aqui
-describe('Teste a função fetchProductsList', () => {
-  it('fetchProductsList é uma função', () => {
-    
+describe('Testar a função fetchProductsList', () => {
+  it('Verifica se fetchProductsList é uma função', () => {
+    expect(typeof fetchProductsList).toBe('function');
   });
+});
 
-  it('fetch é chamado ao executar fetchProductsList', () => {
+it('Verifica se o método fetch foi disparado', async () => {
+  await fetchProductsList('computador');
+  expect(fetch).toHaveBeenCalled();
+});
 
-  });
+it('Verifica se a função retorna "Termo de busca não informado" quando nenhum parâmetro é passado', async () => {
+  expect(async () => await fetchProductsList()).rejects.toThrow('Termo de busca não informado');
+});
 
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
 
-  });
-
-  // it('...', () => {
-  // });
+it('verificar se o endpoint utilizado é `https://api.mercadolibre.com/sites/MLB/search?q=computador`', async () => {
+  await fetchProductsList('computador');
+  expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
 });
