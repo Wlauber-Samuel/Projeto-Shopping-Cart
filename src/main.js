a3fafee4bee73f045516f63ca0dbb787ac2bfe41
Wlauber <wlaubersamuel@gmail.com>
2022-11-30 18:52:24 -0300

    requisito 3, 4 e 5

--- src/main.js
+++ src/main.js
@@ -1,17 +1,43 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
 import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
 const listProducts = document.querySelector('.products');
 const carts = document.querySelector('.cart_products');
-const products = await fetchProductsList('computador');
 
-const loading = () => {
+const addLoading = () => {
   const carregamento = document.createElement('p');
   carregamento.innerHTML = 'Carregando...';
   carregamento.className = 'loading';
   listProducts.appendChild(carregamento);
 };
-loading();
+addLoading();
+
+const removeLoading = () => {
+  const removeL = document.querySelector('.loading');
+  removeL.remove();
+};
+
+const loadingError = () => {
+  carts.className = 'error';
+  carts.innerHTML = 'Algum erro ocorreu, recarregue a página e tente novamente';
+};
+
+const printProductsinScreen = async () => {
+  try {
+    const products = await fetchProductsList('computador');
+    products.forEach((list) => {
+      listProducts.appendChild(createProductElement(list));
+    });
+    removeLoading();
+  } catch (error) {
+    loadingError();
+  }
+};
+
+removeLoading();
+addLoading();
+printProductsinScreen();
